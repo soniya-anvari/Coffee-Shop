1a03b3f01fe34ece32b0b9b70463f2d0d2575b76
soniya <soniyaavdev@gmail.com>
2025-06-29 17:47:10 +0330

    add db file

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import HeroSection from '@/components/templates/index/HeroSection'
 import React from 'react'
 
 function Home() {
   return (
     <div>
+      <HeroSection />
       
     </div>
   )
