de22829164d61eb888a0016bad6a18f046d89d59
soniya <soniyaavdev@gmail.com>
2025-06-30 20:23:16 +0330

    add getStaticProps function in index for fetch menu

---
+++ components/templates/index/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Menu({menuItems}) {
+    console.log(menuItems)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Menu
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,29 @@
 import HeroSection from '@/components/templates/index/HeroSection'
+import Menu from '@/components/templates/index/Menu'
+import { consumeDynamicAccess } from 'next/dist/server/app-render/dynamic-rendering';
 import React from 'react'
 
-function Home() {
+function Home({menuItems}) {
+  
   return (
     <div>
       <HeroSection />
+      <Menu menuItems={menuItems} />
       
     </div>
   )
+}
+export async function getStaticProps(){
+  const res=await fetch('http://localhost:4000/menu');
+  const data=await res.json()
+  console.log({res})
+  console.log('skns')
+    return{
+    props:{
+      menuItems:data
+    },
+    revalidate: 20, 
+  }
 }
 
 export default Home
